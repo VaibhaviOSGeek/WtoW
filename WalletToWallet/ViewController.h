750f0589cdcce9238ccd009dc63c32a69333c12c
harish <harish@Harishs-MacBook-Pro.local>
2014-03-02 19:04:23 +0530

    Initial Commit

---
+++ WalletToWallet/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WalletToWallet
+//
+//  Created by harish on 02/03/2014.
+//  Copyright (c) 2014 Narendra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ WalletToWallet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WalletToWallet
+//
+//  Created by harish on 02/03/2014.
+//  Copyright (c) 2014 Narendra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
