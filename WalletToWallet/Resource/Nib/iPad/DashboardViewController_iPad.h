33688c9adbfe0bde1d19069236b8cff915665210
Narendra <narendrasuthar1989@gmail.com>
2014-03-29 22:02:04 +0530

    dashboard commit
    you can check

---
+++ WalletToWallet/Resource/Nib/iPad/DashboardViewController_iPad.h
@@ -0,0 +1,13 @@
+//
+//  DashboardViewController_iPad.h
+//  WalletToWallet
+//
+//  Created by harish on 29/03/2014.
+//  Copyright (c) 2014 Narendra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashboardViewController_iPad : UIViewController
+
+@end
